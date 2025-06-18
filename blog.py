17604c93d75eb113a30b30050aebee7089f36ee0
EliaOndacs <amirreza.ondacs90@gmail.com>
2025-06-18 12:24:36 +0330

    blog functions and features

---
+++ blog.py
@@ -0,0 +1,21 @@
+# load the configuration
+from src.config import CascadaWebsiteConfig, get_config
+
+
+config: CascadaWebsiteConfig = get_config()
+
+
+def render_markdown(markdown: str):
+    "render markdown to html"
+    from markdown_it import MarkdownIt
+
+    return MarkdownIt().render(markdown)
+
+
+def get_blog_posts():
+    "returns all the blog posts in the following format (name, markdown) for every blog post"
+    for parent, dirs, files in config.blog.walk():
+        for file in files:
+            if file.endswith(".md"):
+                _f = parent / file
+                yield (_f.name.removesuffix(".md"), _f.read_text())
