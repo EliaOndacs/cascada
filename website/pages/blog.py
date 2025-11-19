14fad475f6244411540cec776c1b8a92198248b3
EliaOndacs <amirreza.ondacs90@gmail.com>
2025-11-19 18:32:34 +0330

    fixed css line

--- website/pages/blog.py
+++ website/pages/blog.py
@@ -13,7 +13,7 @@ def Page():
             div(
                 h1(
                     f"#{blog[0]}:",
-                    classname="bg-gradient-to-r from-red-500 to-blue-500 text-transparent bg-clip-text transform origin-left transition-all hover:cursor-default  text-shadow shadow-lg text-3xl",
+                    classname="bg-linear-to-r from-red-500 to-blue-500 text-transparent bg-clip-text transform origin-left transition-all hover:cursor-default  text-shadow shadow-lg text-3xl",
                 ),
                 div(
                     p(render_markdown(blog[1])) if blog[2] == False else blog[1],
@@ -24,7 +24,7 @@ def Page():
                       font-['Inter'] p-6 md:p-10 flex flex-col items-center \
                       leading-relaxed tracking-wide m-4 rounded-3xl \
                       border border-white/5 backdrop-blur-xl \
-                      bg-gradient-to-b from-zinc-900/80 to-black/80 \
+                      bg-linear-to-b from-zinc-900/80 to-black/80 \
                       shadow-[0_0_45px_-5px_rgba(0,0,0,0.3)]",
             )
             for blog in blogs
